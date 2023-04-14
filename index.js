a6b6e032b179f905a1da007ec242e6a479ef23a2
smarieantoine <smarieantoine@hotmail.com>
2023-04-14 22:48:27 +0200

    add process.env.PORT dans index.js

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.all("*", (req, res) => {
   res.status(404).json({ message: "This route does not available !!" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started 🚀🚀🚀!");
 });
