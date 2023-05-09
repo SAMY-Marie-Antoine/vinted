debd5e7cdb014f39719c3c7a76179bc4ad097130
smarieantoine <smarieantoine@hotmail.com>
2023-05-09 17:10:42 +0200

    rajout res.json dans login et message retour email

--- routes/authentification.js
+++ routes/authentification.js
@@ -89,13 +89,12 @@ router.post("/user/login", async (req, res) => {
 
     if (hash !== user[0].hash) {
       return res.status(500).json({ message: "Unauthorized!!" });
-    } else {
-      res.json({
-        _id: user._id,
-        account: user.account,
-        token: user.token,
-      });
     }
+    res.json({
+      _id: user._id,
+      account: user.account,
+      token: user.token,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
